fdf943634a9f5eb0ba776365f6c586f0a2a5250a
Kelly <renkelei.kelly@gmail.com>
2020-12-19 23:34:27 +0200

    updating solution for factorChain.js
    removed log

--- Easy/factorChain.js
+++ Easy/factorChain.js
@@ -18,8 +18,14 @@ factorChain([2, 4, 6, 7, 12]) ➞ false
 factorChain([10, 1]) ➞ false
 */
 
-function factorChain( /*args*/ ) {
-  //your code
+function factorChain(arr) {
+  for (let i = 0; i < arr.length -1; i++) {
+    if (arr[i + 1] % arr[i] !== 0) {
+      console.log("in false",arr[i+1])
+      return false;
+    }
+  }
+  return true;
 }
 
 exports.solution = factorChain;
